fff673d135a464d74cc05600be3e9e91e91715ee
kk0425 <kk0425@gmail.com>
2024-02-18 20:41:45 -0500

    finished lesson

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 function drawGrid() {
-    const tileCount = document.querySelector('#grid-size-value').value;
     const grid = document.querySelector('#grid-container');
+    const tileCount = document.querySelector('#grid-size-value').value;
     let width = (100 / tileCount) + '%';
 
     //clears grid
     document.querySelectorAll(".tile").forEach(tile => tile.remove());
+    
+    function tileColor() {
+        const rainbowMode = document.querySelector('#color-mode-toggle').checked;
+        if (rainbowMode) {
+            function getColorRange() {
+                const min = 100;
+                const max = 255;
+                return Math.floor(Math.random() * (max - min) + min);
+            }
+        
+            const r = getColorRange();
+            const g = getColorRange();
+            const b = getColorRange();
+
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+        return "gray";
+    }
 
     for (let i = 0; i < tileCount ** 2; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add("tile");
         gridSquare.style.width = width;
-
+        
         gridSquare.addEventListener("mouseenter", function() {
-            gridSquare.style.backgroundColor = "gray";
-        },
-        {once: true});
+            gridSquare.style.backgroundColor = tileColor();
+        });
 
         grid.appendChild(gridSquare);
     }
